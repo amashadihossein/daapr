e41f454e5386ffa5d5ceb7c3e59cb07696f690fc
Shawn Balcome <balcomes@gmail.com>
2021-12-02 21:21:11 -0600

    usethis package github action and tidyverse additions.

--- R/daapr-package.R
+++ R/daapr-package.R
@@ -7,5 +7,8 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @import rlang
+#' @importFrom glue glue
+#' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
